d309111d8ca5bfb567af69f865a4792f4ba9aaa8
NG-KWAN-LOK <t106590052@ntut.org.tw>
2020-03-03 19:14:16 +0800

    no message

--- 106590052_HW1/app/src/main/java/com/example/a106590052_hw1/MainActivity.java
+++ 106590052_HW1/app/src/main/java/com/example/a106590052_hw1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a106590052_hw1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
+        Log.d("MainActivity", "This is a debug log.");
+        Log.e("MainActivity", "This is a error log.");
+        Log.w("MainActivity", "This is a warn log.");
+        Log.i("MainActivity", "This is a info log.");
     }
 }
