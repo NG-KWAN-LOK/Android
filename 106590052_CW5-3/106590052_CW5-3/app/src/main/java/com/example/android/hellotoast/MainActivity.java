e0323f61c0ad9cf0355de2f70decdb47e88e0710
NG-KWAN-LOK <t106590052@ntut.org.tw>
2020-04-07 17:39:46 +0800

    no message

---
+++ 106590052_CW5-3/106590052_CW5-3/app/src/main/java/com/example/android/hellotoast/MainActivity.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.hellotoast;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Displays two Buttons and a TextView.
+ * - Pressing the TOAST button shows a Toast.
+ * - Pressing the COUNT button increases the displayed mCount.
+ *
+ * Note: Fixing behavior when device is rotated is a challenge exercise for the student.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.hellotoast.extra.MESSAGE";
+    private ImageView imgCapture;
+    private int Image_Capture_Code = 10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},1);
+        }
+        setContentView(R.layout.activity_main);
+        imgCapture = (ImageView) findViewById(R.id.capturedImage);
+    }
+
+    public void openCamera(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,Image_Capture_Code);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Image_Capture_Code) {
+            if (resultCode == RESULT_OK) {
+                Bitmap bp = (Bitmap) data.getExtras().get("data");
+                imgCapture.setImageBitmap(bp);
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
